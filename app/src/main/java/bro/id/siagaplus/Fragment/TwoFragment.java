ce0908a1dafefc4f93fb4da5df9a79fb916f2c92
aditya rafif <rafifabdurrahman12@gmail.com>
2018-05-10 13:00:38 +0700

    update fragment sg data agenda

--- app/src/main/java/bro/id/siagaplus/Fragment/TwoFragment.java
+++ app/src/main/java/bro/id/siagaplus/Fragment/TwoFragment.java
@@ -135,8 +135,8 @@ public class TwoFragment extends Fragment {
         rawdataAgenda = db.getAllAgenda();
         if (rawdataAgenda.isEmpty()){
             Agenda agenda = new Agenda();
-            agenda.setDate("no upcoming agenda");
-            agenda.setTitle("-");
+            agenda.setDate("-");
+            agenda.setTitle("no upcoming agenda");
             agenda.setId(1);
             mlistAgenda.add(agenda);
         } else if (rawdataAgenda.size() > 3){
